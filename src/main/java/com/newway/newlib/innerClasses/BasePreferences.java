823d5611bb55daa9b3b435f54f06f40b9d9f6240
Behnam Mabodi <gpxchat@gmail.com>
2016-06-13 19:25:27 +0430

    Added more features to Base Preferences

--- src/main/java/com/newway/newlib/innerClasses/BasePreferences.java
+++ src/main/java/com/newway/newlib/innerClasses/BasePreferences.java
@@ -3,6 +3,9 @@ package com.newway.newlib.innerClasses;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.util.Log;
+
+import java.util.Locale;
 
 /**
  * Created by Gold Phoenix on 06/13/2016.
@@ -12,19 +15,47 @@ public class BasePreferences {
     protected static boolean mIsInitialized;
     protected static SharedPreferences mSPrefs;
     protected static boolean mForcePersian = false;
-    protected static Typeface mPersianTypeFace;
+    protected static Typeface mTypeFaceMediumPersian;
+    protected static Typeface mTypeFaceMediumEnglish;
+    protected static Typeface mTypeFaceRegularEnglish;
 
-    public BasePreferences(Context context, String fontAssetLocation) {
+    public static int TYPEFACE_PERSIAN = 10;
+    public static int TYPEFACE_REGULAR = 10;
+    public static int TYPEFACE_MEDIUM = 20;
+    public static int TYPEFACE_BOLD = 30;
+
+
+
+    public BasePreferences(Context context, String fontAssetLocationPersian) {
         if (!mIsInitialized) {
             mSPrefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
-            if(fontAssetLocation != null)
-                mPersianTypeFace = Typeface.createFromAsset(context.getAssets(), fontAssetLocation);
+
+            if (fontAssetLocationPersian != null)
+                mTypeFaceMediumPersian = Typeface.createFromAsset(context.getAssets(), fontAssetLocationPersian);
+            mTypeFaceMediumEnglish = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Medium.ttf");
+            mTypeFaceRegularEnglish = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Regular.ttf");
+
+            //        Log.d("Display LANG", Locale.getDefault().getDisplayLanguage());
             mIsInitialized = true;
         }
     }
 
-    public Typeface getTypeface() {
-        return mPersianTypeFace;
+    public Typeface getTypeface(int typeFacesize) {
+        if (isForcedPersian() || Locale.getDefault().getDisplayLanguage().equals("فارسی"))
+            return mTypeFaceMediumPersian;
+        else
+            return mTypeFaceMediumEnglish;
+    }
+
+    public Typeface getTypeface(int typeFaceLangID, int typeFacesize) {
+        if (typeFaceLangID == TYPEFACE_PERSIAN)
+            return mTypeFaceMediumPersian;
+        else {
+            if(typeFacesize == TYPEFACE_REGULAR)
+                return mTypeFaceRegularEnglish;
+            else
+                return mTypeFaceMediumEnglish;
+        }
     }
 
     public boolean isForcedPersian() {
