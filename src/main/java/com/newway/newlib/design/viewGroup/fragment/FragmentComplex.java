f61f0f6ce961a7be8135da14a927a2e2048ee9b1
Behnam Mabodi <gpxchat@gmail.com>
2017-01-31 14:47:55 +0330

    @ Changed Drawer Management Behavior To make it simpler

--- src/main/java/com/newway/newlib/design/viewGroup/fragment/FragmentComplex.java
+++ src/main/java/com/newway/newlib/design/viewGroup/fragment/FragmentComplex.java
@@ -1,55 +1,26 @@
 package com.newway.newlib.design.viewGroup.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by goldm on 30/01/2017.
  */
 
 public class FragmentComplex extends Fragment {
-    private Toolbar mToolbar;
-    private DrawerLayout mDrawerLayout;
-    private AppCompatActivity mActivity;
-    private int mToolbarId;
-    private int mOpenDrawerString;
-    private int mCloseDrawerString;
 
     public FragmentComplex() {
-        // It's recommended to be singleton
     }
 
-    public void initialize(DrawerLayout drawerLayout, int toolbarId, int openDrawerString, int closeDrawerString) {
-        mDrawerLayout = drawerLayout;
-        mToolbarId = toolbarId;
-        mOpenDrawerString = openDrawerString;
-        mCloseDrawerString = closeDrawerString;
-
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        mActivity = (AppCompatActivity) getActivity();
-        mToolbar = (Toolbar) getView().findViewById(mToolbarId);
-        mActivity.setSupportActionBar(mToolbar);
+    protected void addDrawerSyncToggle(DrawerLayout drawerLayout, Toolbar toolbar, int id_string_drawer_open, int id_string_drawer_closed) {
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                mActivity, mDrawerLayout, mToolbar, mOpenDrawerString, mCloseDrawerString);
-        mDrawerLayout.addDrawerListener(toggle);
-        super.onViewCreated(view, savedInstanceState);
+                getActivity(), drawerLayout, toolbar, id_string_drawer_open, id_string_drawer_closed);
+        drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
     }
+
 }
