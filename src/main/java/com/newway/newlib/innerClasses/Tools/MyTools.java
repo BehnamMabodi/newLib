5afbac058c0a08353f5abb6e9f8250a66257ee91
Behnam Mabodi <gpxchat@gmail.com>
2017-02-11 16:20:03 +0330

    + Added Persian Digits Converter

--- src/main/java/com/newway/newlib/innerClasses/Tools/MyTools.java
+++ src/main/java/com/newway/newlib/innerClasses/Tools/MyTools.java
@@ -45,6 +45,32 @@ public abstract class MyTools {
         public static float pxToDp(float px) {
             return SystemTools.pxToDp(px);
         }
+
+        public static String NumberToPersian(String str) {
+            char[] persianChars = {'۰', '١', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '٩'};
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < str.length(); i++) {
+                if (Character.isDigit(str.charAt(i))) {
+                    try {
+                        builder.append(persianChars[(int) (str.charAt(i)) - 48]);
+                    } catch (Exception e) {
+                        builder.append(str.charAt(i));
+                    }
+                } else {
+                    builder.append(str.charAt(i));
+                }
+
+                // if else must enabled if character is not digits
+            }
+            return builder.toString();
+        }
+
+        public static String NumberToPersian(String str, boolean convert) {
+            if (convert)
+                return NumberToPersian(str);
+            else
+                return str;
+        }
     }
 
 
