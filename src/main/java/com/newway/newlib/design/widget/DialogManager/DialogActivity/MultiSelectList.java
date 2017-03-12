89aba3be64888e1ebe32604370e3264f5e1e9d02
Behnam Mabodi <gpxchat@gmail.com>
2017-03-12 17:22:46 +0330

    + Connection Between Adapter and Boolean Array Created

--- src/main/java/com/newway/newlib/design/widget/DialogManager/DialogActivity/MultiSelectList.java
+++ src/main/java/com/newway/newlib/design/widget/DialogManager/DialogActivity/MultiSelectList.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import com.newway.newlib.R;
 import com.newway.newlib.design.widget.view.CheckBoxStyleable;
@@ -16,6 +17,7 @@ public class MultiSelectList extends DialogActivity {
     RecyclerView mRecyclerView;
     boolean[] mCheckListValues;
     String[] mItemText;
+    CheckBoxStyleable.OnCheckedChangeListener mOnCheckedChangeListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,14 +35,23 @@ public class MultiSelectList extends DialogActivity {
             mCheckListValues = SavedBundle.getBooleanArray("checked_items");
 
         mRecyclerView = new RecyclerView(this);
-        RecyclerView.LayoutParams params = new RecyclerView.LayoutParams((int) getResources().getDimension(R.dimen.dialog_activity_min_width), ViewGroup.LayoutParams.WRAP_CONTENT);
+        RecyclerView.LayoutParams params = new RecyclerView.LayoutParams(
+                (int) getResources().getDimension(R.dimen.dialog_activity_min_width),
+                (int) getResources().getDimension(R.dimen.dialog_activity_min_height));
         mRecyclerView.setLayoutParams(params);
         mFrameLayout.addView(mRecyclerView);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
         mRecyclerView.setAdapter(new Adapter());
 
-
+        mOnCheckedChangeListener = new CheckBoxStyleable.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                ViewHolder holder = (ViewHolder) mRecyclerView.findContainingViewHolder(compoundButton);
+                if (holder != null)
+                    mCheckListValues[holder.getAdapterPosition()] = b;
+            }
+        };
     }
 
     @Override
@@ -54,6 +65,7 @@ public class MultiSelectList extends DialogActivity {
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             CheckBoxStyleable checkBoxStyleable = (CheckBoxStyleable) LayoutInflater.from(parent.getContext()).inflate(R.layout.dialog_activity_multi_select_item_layout, parent, false);
+            checkBoxStyleable.setOnCheckedChangeListener(mOnCheckedChangeListener);
             return new ViewHolder(checkBoxStyleable);
         }
 
